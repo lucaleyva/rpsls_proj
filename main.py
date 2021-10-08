a6bc87e55a1bf054c8d2346dd0ebb13f34febf50
Luca Leyva <imlucaleyva@gmail.com>
2021-10-08 16:52:24 -0400

    Added lists of results for gesture combinations: tie, win, lose.

--- main.py
+++ main.py
@@ -2,4 +2,9 @@ import random
 
 def play():
     user = input("What's your choice? 'r' for rock, 'p' for paper, 's' for scissors\n")
-    
+
+
+
+# gestures = ["rock", "paper", "scissors", "lizard", "spock"]
+
+
--- player.py
+++ player.py
@@ -5,4 +5,8 @@ class Player:
         pass
 
     # Common things between user and AI.
-gestures = ["rock", "paper", "scissors", "lizard", "spock"]
+# gestures = ["rock", "paper", "scissors", "lizard", "spock"]
+
+tie_results = [("rock","rock"), ("paper","paper"), ("scissors","scissors"), ("lizard","lizard"), ("spoke","spoke")]
+win_results = [("rock","scissors"),("rock","lizard"),("paper","spock"),("scissors","paper"),("scissors","lizard"),("lizard","spock"),("lizard","paper"),("spock","scissors"),("spock","rock")]
+lose_results = [("rock","spock"),("rock","paper"),("paper","scissors"),("paper","lizard"),("scissors","spock"),("scissors","rock"),("lizard","scissors"),("lizard","rock"),("spock","lizard"),("spock","paper")]
