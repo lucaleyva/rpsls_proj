7aa1670fe49439cc7b17966a3c581fb7c40ea764
Ben Fischer <fischerben010@gmail.com>
2021-10-08 16:34:24 -0500

    Here is an updated  version that has a loose outline of how the game would flow

---
+++ gameboard.py
@@ -0,0 +1,26 @@
+class Gameboard:
+    def __init__(self):
+        pass
+
+    def display_welcome(self):
+        print('Welcome to Rock, Paper, Scissors, Lizard and Spock!')
+        print('The rules are the same as traditional Rock Paper Scissors but adding Lizard and Spock. Here are all the combinations: Rock crushes Scissors, Scissors cuts Paper, Paper covers Rock, Rock crushes, Lizard, Lizard poisons Spock, Spock smashes Scissors, Scissors decapitates Lizard')
+        # we can worry about the looks of this later
+
+    def choose_gametype(self):
+        game_type = input(
+            "Please select a game type: '1' for single user or '2' for multiplayer")
+        if game_type == 1:
+            # print('you have chosen to play against a computer)
+            pass
+            # set up user vs computer
+        elif game_type == 2:
+            # print(you have chosen to play against another user)
+            # set up user vs user
+            pass
+        else:
+            self.choose_gametype
+
+        def display_throw_options(self):
+            user_input = input(
+                "Please choose your throw 'rock', 'paper', 'scissors', lizard or spock")
