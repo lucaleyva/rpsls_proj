f547f72fa8ce9349e9e5c3e943556027aec20d88
Kendall Jacobs <kendalljjacobs@icloud.com>
2021-10-11 11:16:38 -0500

    Added the conditions for the player to play against the computer. Updated the player input to return lowercase.

--- computer.py
+++ computer.py
@@ -1,10 +1,10 @@
-from player import Player
+# from player import Player
 from random import randint
 
-class Computer(Player):
-    def __init__(self):
-        super().__init__()
-    pass
+# class Computer(Player):
+#     def __init__(self):
+#         super().__init__()
+#     pass
 
 
 # will randomly generate choice
@@ -25,4 +25,33 @@ elif rand_number == 4:
 print(f" Computer chose {computer}")
         
 
+if player == computer:
+	print("It's a tie!")
+elif player == "rock":
+	if computer == "scissors" or computer == "lizard":
+		print("player wins!")
+	elif computer == "paper" or computer == "spock":
+		print("computer wins!")
+elif player == "paper":
+	if computer == "rock" or computer == "spock":
+		print("player wins!")
+	elif computer == "scissors" or computer == "lizard":
+		print("computer wins!")
+elif player == "scissors":
+	if computer == "paper" or computer == "lizard":
+		print("player wins!")
+	elif computer == "rock" or computer == "spock":
+		print("computer wins!")	
+elif player == "lizard":
+	if computer == "paper" or computer == "spock":
+		print("player wins!")
+	elif computer == "rock" or computer == "scissors":
+		print("computer wins!")	
+elif player == "spock":
+	if computer == "rock" or computer == "scissors":
+		print("player wins!")
+	elif computer == "paper" or computer == "lizard":
+		print("computer wins!")	
+else:
+	print("something went wrong")
 
